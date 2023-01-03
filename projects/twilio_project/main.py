2fb836ecffaf6a6864be50e4ebbf2e36700badf5
Alexis <ala1289@gmail.com>
2023-01-02 20:36:23 -0600

    Get current hour and date, hour

--- projects/twilio_project/main.py
+++ projects/twilio_project/main.py
@@ -19,4 +19,10 @@ api_key = os.getenv('API_KEY_WAPI')
 # Armado de la URL
 url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
 response = requests.get(url_clima).json()
+## Get the current hour in 24 hour format
+current_hour = datetime.now().strftime("%H")
+# get date from response
+date = response['forecast']['forecastday'][0]['hour'][0]['time'].split()[0]
+# get hour from response
+hour = int(response['forecast']['forecastday'][0]['hour'][int(current_hour)]['time'].split()[1].split(':')[0])
 breakpoint()
