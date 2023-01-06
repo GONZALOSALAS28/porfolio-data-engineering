4ddf9dac26621a03f2e67ad762f1e4b3a2b5884b
Alexis <ala1289@gmail.com>
2023-01-06 18:42:37 +0000

    Show map

--- projects/streamlit/app.py
+++ projects/streamlit/app.py
@@ -69,7 +69,30 @@ if app_mode == 'Run App':
     choose_products =  c3.selectbox("Choose Oil", products)
 
     if c3.button('SHOW MAP'):
-      pass
+      gdf_stores_results2 = gdf_stores_results[gdf_stores_results['Producto']==choose_products]
+      gdf_stores_results2 = gdf_stores_results2.reset_index()
+      gdf_stores_results2 = gdf_stores_results2.drop(columns = 'index')
+      icono = "usd"
+
+      m = folium.Map([geo_source[0],geo_source[1]], zoom_start=15)
+
+      # Circle
+      folium.Circle(
+      radius=int(rad)*1000,
+      location=[geo_source[0],geo_source[1]],
+      color='green',
+      fill='red').add_to(m)
+
+      # Centroid
+      folium.Marker(location=[geo_source[0],geo_source[1]],
+                          icon=folium.Icon(color='black', icon_color='white',
+                          icon="home", prefix='glyphicon')
+                          ,popup = "<b>CENTROID</b>").add_to(m)
+
+      marker_rest(gdf_stores_results2,m,unit,choose_products,icono)
+
+      # call to render Folium map in Streamlit
+      folium_static(m)
 
 elif app_mode == "About Me":
   pass
