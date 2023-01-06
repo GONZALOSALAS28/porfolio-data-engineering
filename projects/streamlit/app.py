a60a1ddedbf3777005f83ed80a1e969c5d262d2f
Alexis <ala1289@gmail.com>
2023-01-06 18:17:38 +0000

    Add functionality input and validation

--- projects/streamlit/app.py
+++ projects/streamlit/app.py
@@ -30,7 +30,25 @@ st.sidebar.image(image , caption="Nearby Oil App",width = 256)
 app_mode = st.sidebar.selectbox("Choose app mode", ["Run App","About Me"])
 
 if app_mode == 'Run App':
-  pass
+  st.title('Nearby Oil Station App')
+  st.markdown('App Description')
+
+  df_map = pd.read_csv('DF_STATIONS.csv')
+  cities =  list(df_map['Municipio'].unique())
+
+  # Crear columnas usar st.columns especificando el ancho de las columnas
+  c1,c2,c3,c4,c5 = st.columns((1,6,6,6,1))
+
+  choose_city =  c2.selectbox("Choose city", cities)
+
+  central_location = c2.text_input('Central Location', 'CC Multiplaza , Bogotá')
+
+  DEVELOPER_KEY = os.getenv('API_KEY')
+
+  if len(central_location) != 0 :
+    R = GetLatLon2(central_location,DEVELOPER_KEY)
+    geo
+    breakpoint()
 
 elif app_mode == "About Me":
   pass
